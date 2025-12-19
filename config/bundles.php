745c23be82b80f820c77ab566af8d27fb52819bc
Kevin Papst <kevin@kevinpapst.de>
2025-12-19 17:39:43 +0100

    remove maker bundle

--- config/bundles.php
+++ config/bundles.php
@@ -8,5 +8,4 @@ return [
     KevinPapst\TablerBundle\TablerBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
