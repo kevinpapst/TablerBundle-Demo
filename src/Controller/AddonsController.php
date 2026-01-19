711a23c166c1672e7466e13940acac036f8f043f
CAVASIN Florian <florian.cavasin@proton.me>
2026-01-19 09:50:21 +0100

    [addons] Add menu

---
+++ src/Controller/AddonsController.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the Tabler-Bundle demo.
+ * Copyright 2021 Kevin Papst - www.kevinpapst.de
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AddonsController extends AbstractController
+{
+
+}
